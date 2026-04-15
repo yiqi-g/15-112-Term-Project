b056560aaf4a39e28bc09493497b83f842b857b4
yiqi-g <yiqig@andrew.cmu.edu>
2026-04-15 19:43:04 -0400

    intialized main.py, set up cmu graphics

---
+++ main.py
@@ -0,0 +1,14 @@
+from cmu_graphics import *
+from cmu_cpcs_utils import *
+import math
+
+def onAppStart(app):
+
+def reDrawAll(app):
+
+def onMouseDrag(app, mouseX, mouseY):
+
+def main():
+    runApp()
+
+main()
